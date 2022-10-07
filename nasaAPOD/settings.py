5a517ca938e08c7d97ea8b39f941bf9f050945e9
Daniel-olaO <danieladedeji135@gmail.com>
2022-10-07 11:13:12 -0400

    User athentication methhod added

--- nasaAPOD/settings.py
+++ nasaAPOD/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'api.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
