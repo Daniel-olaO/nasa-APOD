4bbd141e432cc47acea10ddef42faa18e83bce07
Daniel-olaO <danieladedeji135@gmail.com>
2022-11-05 15:21:24 -0400

    Added toggle-subscription route

--- api/models.py
+++ api/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
     email = models.CharField(max_length=255, unique=True)
     phone = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=255)
+    isSubscribed = models.BooleanField(default=True)
     username = None
 
     USERNAME_FIELD = 'email'
--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ import datetime
 import jwt
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from rest_framework.exceptions import AuthenticationFailed
 from .serializers import UserSerializer
 from .models import User
@@ -75,3 +76,19 @@ class LogoutView(APIView):
             'message': 'success'
         }
         return response
+class SubscriptionView(APIView):
+    #toggle isSubscription
+    def post(self, request, id):
+        user = User.objects.filter(id=id).first()
+        user.isSubscribed = not user.isSubscribed
+        user.save()
+        if user.isSubscribed:
+            return Response({'name':user.email,
+                            'isSubscribed':user.isSubscribed,
+                            'message': 'You are now subscribed to NASA APOD Texting Service'
+                            })
+        else:
+            return Response({'name':user.email,
+                            'isSubscribed':user.isSubscribed,
+                            'message': 'You are now unsubscribed from NASA APOD Texting Service'
+                            })
