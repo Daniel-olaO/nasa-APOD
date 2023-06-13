af95622f5f1d2a7124bcfde3ef3b929db048680f
Daniel-olaO <88808467+Daniel-olaO@users.noreply.github.com>
2023-06-12 20:40:10 -0400

    Update scheduler.py

--- api/scheduler/scheduler.py
+++ api/scheduler/scheduler.py
@@ -6,6 +6,6 @@ from ..views import sendAPOD
 def start():
     scheduler = BackgroundScheduler()
     # schedule the sendAPOD function to run every day at 12:05:00
-    scheduler.add_job(sendAPOD, 'cron', hour=0,minute=39,second=0)
+    scheduler.add_job(sendAPOD, 'cron', hour=12,minute=5,second=0)
     print("running scheduled job........", datetime.datetime.now()) 
     scheduler.start()
