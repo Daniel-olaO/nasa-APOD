b1b7150fc5423f5b40e893d2915b24bd65593a41
daniel <danieladedeji997@yahoo.com>
2023-06-27 14:25:50 -0400

    Fixed Trigger

--- api/scheduler/scheduler.py
+++ api/scheduler/scheduler.py
@@ -1,18 +1,14 @@
 import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
-from ..views import sendAPOD, storeMessageWeekly, sendBackUpMessage
+from ..views import sendAPOD, storeMessageWeekly
 
 
 def start():
     scheduler = BackgroundScheduler()
     # schedule the sendAPOD function to run every day at 12:05:00
-    scheduler.add_job(sendAPOD, 'cron', hour=12,minute=5,second=0)
-    scheduler.add_job(storeMessageWeekly, trigger=CronTrigger(day_of_week='mon', hour=9, minute=37, second=0)) 
+    scheduler.add_job(sendAPOD, 'cron', hour=12, minute=5, second=0)
+    scheduler.add_job(storeMessageWeekly, 'cron', day_of_week='mon', hour=13, minute=5, second=0)
     print("running scheduled job........", datetime.datetime.now()) 
 
-    try:
-        scheduler.start()
-    except Exception as exception:
-        scheduler.shutdown()
-        print("Scheduler stopped")
+    scheduler.start()
