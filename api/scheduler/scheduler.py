d88576be09d2af45a1b02216891b8a24b34aea72
Daniel-olaO <88808467+Daniel-olaO@users.noreply.github.com>
2023-10-23 07:58:31 -0400

    Update scheduler.py

--- api/scheduler/scheduler.py
+++ api/scheduler/scheduler.py
@@ -6,9 +6,7 @@ from ..views import send_NASA_APOD, store_message_weekly
 
 def start():
     scheduler = BackgroundScheduler()
-    # schedule the sendAPOD function to run every day at 12:05:00
     scheduler.add_job(send_NASA_APOD, 'cron', hour=12, minute=5, second=0)
-    # schedule the store_message_weekly function to run every monday at 13:05:00
     scheduler.add_job(store_message_weekly, 'cron', day_of_week='mon', hour=13, minute=5, second=0)
     print("running scheduled job........", datetime.datetime.now()) 
 
